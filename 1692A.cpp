00632044d7b1cfaa13014df52ecb2706dc8dcebd
Willzera <113561581+Wssfilho@users.noreply.github.com>
2025-09-19 17:26:26 -0300

    otimization: otimization code to more legible

--- 1692A.cpp
+++ 1692A.cpp
@@ -1,28 +1,16 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int t; cin >> t;
-    while(t--)
-    {
-        int k = 0;
-        int a; int b; int c; int d;
-        cin >> a; cin >> b; cin >> c; cin >>d ;
-        if(a < b)
-        {
-            k++;
-        } 
-        if (a < c)
-        {
-            k++;
-        }
-        if(a < d)
-        {
-            k++;
-        }
-        cout << k << '\n';
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << (a < b) + (a < c) + (a < d) << '\n';
     }
 }
